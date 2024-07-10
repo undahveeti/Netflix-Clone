f4cf9e566bb7286479f6f63b7156784461d51469
Andy Tran <undahvt@gmail.com>
2024-07-10 14:22:34 -0700

    flex direction and div for centering

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Inner } from './styles/jumbotron';
 
 export default function Jumbtron({}){
-    return <p>I am the Jumbo!</p>
+    return (
+        <Inner>
+            <p>Hello again!</p>
+        </Inner>
+    )
 }
